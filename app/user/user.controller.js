820cb34ec25b0762a36b9ce919013d10b0996726
lazlanrafar <lazlanrafar@gmail.com>
2023-05-07 06:09:30 +0700

    fetch user by id

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -1,10 +1,21 @@
 const { InternalServerError, Ok } = require("../../utils/http-response");
-const { FetchUser } = require("./user.repository");
+const { FetchUser, FetchUserById } = require("./user.repository");
 
 module.exports = {
   GetUser: async (req, res) => {
     try {
       const result = await FetchUser();
+
+      return Ok(res, result, "User fetched successfully");
+    } catch (error) {
+      return InternalServerError(res, error, "Failed to get user");
+    }
+  },
+  GetUserById: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const result = await FetchUserById(id);
 
       return Ok(res, result, "User fetched successfully");
     } catch (error) {
--- app/user/user.route.js
+++ app/user/user.route.js
@@ -1,8 +1,9 @@
-const { GetUser } = require("./user.controller");
+const { GetUser, GetUserById } = require("./user.controller");
 
 const express = require("express");
 const router = express.Router();
 
 router.get("/", GetUser);
+router.get("/:id", GetUserById);
 
 module.exports = router;
