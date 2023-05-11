1639c5d9f319d19e68b620aea2587ec03b716f4b
lazlanrafar <lazlanrafar@gmail.com>
2023-05-11 23:32:54 +0700

    fetch gaji

--- app/gaji/gaji.Controller.js
+++ app/gaji/gaji.Controller.js
@@ -1,7 +1,21 @@
+const moment = require("moment/moment");
 const { InternalServerError, Ok } = require("../../utils/http-response");
-const { StoreGaji } = require("./gaji.Repository");
+const { StoreGaji, FetchGaji } = require("./gaji.Repository");
 
 module.exports = {
+  GetGaji: async (req, res) => {
+    try {
+      const result = await FetchGaji();
+
+      result.forEach((item) => {
+        item.tanggal = moment(result.tanggal).format("YYYY-MM");
+      });
+
+      return Ok(res, result, "Gaji fetched successfully");
+    } catch (error) {
+      return InternalServerError(res, error, "Failed to create gaji");
+    }
+  },
   CreateGaji: async (req, res) => {
     try {
       await StoreGaji(req.body);
--- app/gaji/gaji.Repository.js
+++ app/gaji/gaji.Repository.js
@@ -2,6 +2,18 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = {
+  FetchGaji: async () => {
+    return await prisma.tbl_gaji.findMany({
+      include: {
+        user: {
+          select: {
+            nip: true,
+            nama: true,
+          },
+        },
+      },
+    });
+  },
   StoreGaji: async (payload) => {
     return await prisma.tbl_gaji.create({
       data: payload,
