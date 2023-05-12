933a7351df4dd028440e6a6eb4b82fee7dac244b
lazlanrafar <lazlanrafar@gmail.com>
2023-05-12 13:46:44 +0700

    add tanggal spm uang lembur

--- app/uang-lembur/uang-lembur.Middleware.js
+++ app/uang-lembur/uang-lembur.Middleware.js
@@ -4,6 +4,7 @@ module.exports = {
   CreateUangLemburMiddleware: (req, res, next) => {
     try {
       req.body.tanggal = new Date(req.body.tanggal);
+      req.body.tanggal_spm = new Date(req.body.tanggal_spm);
       req.body.jam_kerja = parseInt(req.body.jam_kerja);
       req.body.jam_libur = parseInt(req.body.jam_libur);
       req.body.jam_makan = parseInt(req.body.jam_makan);
