75b66bcc8997374ca1a1f208cf2de8ddf7a6f998
lazlanrafar <lazlanrafar@gmail.com>
2023-05-12 13:03:48 +0700

    setup uang lembur

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const userRoute = require("../app/user/user.route");
 const gajiRoute = require("../app/gaji/gaji.Route");
 const tunjanganRoute = require("../app/tunjangan/tunjangan.Route");
 const uangMakanRoute = require("../app/uang-makan/uang-makan.Route");
+const uangLemburRoute = require("../app/uang-lembur/uang-lembur.Route");
 
 module.exports = function (app) {
   const apiVersion = process.env.API_VERSION || "v1";
@@ -18,6 +19,7 @@ module.exports = function (app) {
   app.use(`${preRoute}/gaji`, AuthToken, gajiRoute);
   app.use(`${preRoute}/tunjangan`, AuthToken, tunjanganRoute);
   app.use(`${preRoute}/uang-makan`, AuthToken, uangMakanRoute);
+  app.use(`${preRoute}/uang-lembur`, AuthToken, uangLemburRoute);
 
   app.get(`${preRoute}/whoIs`, AuthToken, (req, res) => {
     res.json({ user: req.user });
